e2e08d268b2da5fac4452428f4ed56837ee779a3
garcialopezmanel <mgarcia163@uoc.edu>
2026-05-02 17:43:07 +0200

    finalizado

--- 4c/ej4c4.py
+++ 4c/ej4c4.py
@@ -55,28 +55,45 @@ Exemple:
          "Polly"        
 """
 # Write abstract class Animal here
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def talk(self):
+        pass
 
 # Corret and overwrite class Dog(Animal) here 
-class Dog():
+class Dog(Animal):
     def __init__(self, name):
         self.name = name
-
     def talk(self):
-        pass
+        return "¡Guau!"
 
 # Corret and overwrite class Cat(Animal) here 
-class Cat():
+class Cat(Animal):
     def __init__(self, name):
         self.name = name
     def talk(self):
-        pass
+        return "¡Meow!"
 
 # Corret and overwrite class Parrot(Animal) here 
-class Parrot():
+class Parrot(Animal):
     def __init__(self, name):
         self.name = name
     def talk(self):
-        pass
+        return "¡Whistle!"
+    
+dog = Dog("Fido")
+dog.talk()
+
+cat = Cat("Fido")
+cat.talk()
+
+parrot = Parrot("Polly")
+parrot.name = ""
 
 
 
